dd32668fcdb2f50f07fb451ed3636f601cf3299d
Josué Moreau <jsmr14.12@gmail.com>
2021-05-17 18:01:23 +0200

    test création d'un module pour l'audio

---
+++ Expérimentations/test_audio/audio_exp.py
@@ -0,0 +1,33 @@
+from IPython.display import display
+import base64
+import jp_proxy_widget
+
+class Audio():
+    def __init__(self, filename):
+        self.widget = jp_proxy_widget.JSProxyWidget()
+        self.js = "../../howler.js"
+        
+        self.widget.load_js_files([self.js])
+        
+        f = open(filename, 'rb')
+        data = f.read()
+        encoded = base64.b64encode(data)
+        
+        self.url = 'data:audio/x-wav;base64,' + str(encoded)[2:-1]
+        
+        self.widget.js_init("""
+            element.empty();
+
+            element.sound = new Howl({
+                src: [url]
+            });
+            """, url=self.url)
+        
+    def display(self):
+        display(self.widget)
+        
+    def play(self):
+        self.widget.element.sound.play()
+        
+    def pause(self):
+        self.widget.element.sound.pause()
