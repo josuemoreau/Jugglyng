e4b6bebb440c04d8ed595b81be5719fa2e79df00
Josué Moreau <jsmr14.12@gmail.com>
2021-05-17 10:58:45 +0200

    essais de modification de la classe Audio

--- audio.py
+++ audio.py
@@ -1,35 +1,65 @@
 from IPython.display import display, HTML, Javascript
 
+# <script type="module">
+
+
+# Init section
+init_script = """<script type="module">
+import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
+
+const camera = new THREE.Camera();
+
+const scene = new THREE.Scene();
+
+const listener = new THREE.AudioListener();
+camera.add(listener);
+
+window.THREE = THREE;
+window.listener = listener;
+</script>"""
+display(HTML(init_script))
+
+js_script_model = """<script>
+const sound{id} = new THREE.Audio(listener);
+const audioLoader{id} = new THREE.AudioLoader();
+
+audioLoader{id}.load('{file}', function(buffer) {{
+    sound{id}.setBuffer(buffer);
+    sound{id}.setLoop(false);
+    sound{id}.setVolume(0.5);
+}});
+
+function play{id}() {{
+    sound{id}.play();
+}}
+
+window.play{id} = play{id}
+</script>"""
+
+
+def new_id_fun():
+    n = 0
+
+    def f():
+        nonlocal n
+        n += 1
+        return n
+    return f
+
+
+new_id = new_id_fun()
+
+
 class Audio():
-    js_script_begin = """<script type="module">
-		import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
-		
-		const camera = new THREE.Camera();
-
-		const scene = new THREE.Scene();
-
-		// create an AudioListener and add it to the camera 
-		const listener = new THREE.AudioListener();
-		camera.add(listener); // create a global audio source
-
-		const sound = new THREE.Audio(listener); // load a sound and set it as the Audio object's buffer 
-
-		const audioLoader = new THREE.AudioLoader();
-		audioLoader.load('"""
-    
-    js_script_end = """', function(buffer) {
-			sound.setBuffer(buffer);
-			sound.setLoop(false);
-			sound.setVolume(0.5);
-			sound.play();
-		});
-    </script>"""
-    
     def __init__(self, file):
         self.file = file
-        self.js_script = self.js_script_begin + file + self.js_script_end
-        self.html = HTML(self.js_script)
-        self.disp = display("", display_id = True)
-        
+        self.id = new_id()
+
+        self.js_script_init = js_script_model.format(id=self.id, file=file)
+        display(HTML(self.js_script_init))
+
+        self.html = HTML('<script>play%d();</script>' % self.id)
+        self.disp = display("", display_id=True)
+
     def play(self):
-        self.disp.update(self.html)
+        self.disp.update(Javascript("play{id}();".format(id=self.id)))
