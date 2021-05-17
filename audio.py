ad3b6e2883c95796919559fb88cdda97ac26f427
Josué Moreau <jsmr14.12@gmail.com>
2021-05-17 18:03:23 +0200

    mise à jour du module audio

--- audio.py
+++ audio.py
@@ -1,22 +1,34 @@
-from IPython.display import display, HTML, Javascript
+from IPython.display import display
+import base64
+import jp_proxy_widget
+
 
 class Audio():
-    js_script_begin = """<script src="howler.js"></script>
-		<script>
-    		var sound = new Howl({
-      			src: ['"""
-    
-    js_script_end = """']
-			});
-			
-			sound.play()
-		</script>"""
-    
-    def __init__(self, file):
-        self.file = file
-        self.js_script = self.js_script_begin + file + self.js_script_end
-        self.html = HTML(self.js_script)
-        self.disp = display("", display_id = True)
-        
+    def __init__(self, filename):
+        self.widget = jp_proxy_widget.JSProxyWidget()
+        self.js = "../../howler.js"
+
+        self.widget.load_js_files([self.js])
+
+        f = open(filename, 'rb')
+        data = f.read()
+        encoded = base64.b64encode(data)
+
+        self.url = 'data:audio/x-wav;base64,' + str(encoded)[2:-1]
+
+        self.widget.js_init("""
+            element.empty();
+
+            element.sound = new Howl({
+                src: [url]
+            });
+            """, url=self.url)
+
+    def display(self):
+        display(self.widget)
+
     def play(self):
-        self.disp.update(self.html)
+        self.widget.element.sound.play()
+
+    def pause(self):
+        self.widget.element.sound.pause()
