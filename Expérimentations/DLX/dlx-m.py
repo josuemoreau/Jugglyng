da732d9f985bc3135ea04cfb2027a44be7b8799d
Josué Moreau <jsmr14.12@gmail.com>
2021-06-03 11:20:08 +0200

    correction de bug + _ pour les fcts à ne pas utiliser hors du module

--- Expérimentations/DLX/dlx-m.py
+++ Expérimentations/DLX/dlx-m.py
@@ -4,46 +4,46 @@ import cppyy
 cppyy.include('dlx-m.hpp')
 cppyy.load_library('dlx-m.so')
 
-std = cppyy.gbl.std
-DLX_M = cppyy.gbl.DLX_M
-INT = DLX_M.INT
-COLOR = DLX_M.COLOR
-DLX = DLX_M.DLX
-EMPTY_COLOR = 0
-AbstrItem = DLX_M.AbstrItem
+_std = cppyy.gbl.std
+_DLX_M = cppyy.gbl.DLX_M
+_INT = _DLX_M.INT
+_COLOR = _DLX_M.COLOR
+_DLX = _DLX_M.DLX
+_EMPTY_COLOR = 0
+_AbstrItem = _DLX_M.AbstrItem
 
-primary_tpl = std.make_tuple['DLX_M::AbstrItem*', INT, INT]
-primary_vct = std.vector[std.tuple['DLX_M::AbstrItem*', INT, INT]]
-secondary_vct = std.vector['DLX_M::AbstrItem*']
+_primary_tpl = _std.make_tuple['DLX_M::AbstrItem*', _INT, _INT]
+_primary_vct = _std.vector[_std.tuple['DLX_M::AbstrItem*', _INT, _INT]]
+_secondary_vct = _std.vector['DLX_M::AbstrItem*']
 
-row_primary_vct = std.vector['DLX_M::AbstrItem*']
-row_secondary_tpl = std.make_tuple['DLX_M::AbstrItem*', COLOR]
-row_secondary_vct = std.vector[std.tuple['DLX_M::AbstrItem*', COLOR]]
-row_tpl = std.make_tuple[row_primary_vct, row_secondary_vct]
-row_vct = std.vector[std.tuple[row_primary_vct, row_secondary_vct]]
-
-
-def P(x: List[Tuple[AbstrItem, int, int]]):
-    return primary_vct([primary_tpl(obj, low, high) for (obj, low, high) in x])
+_row_primary_vct = _std.vector['DLX_M::AbstrItem*']
+_row_secondary_tpl = _std.make_tuple['DLX_M::AbstrItem*', _COLOR]
+_row_secondary_vct = _std.vector[_std.tuple['DLX_M::AbstrItem*', _COLOR]]
+_row_tpl = _std.make_tuple[_row_primary_vct, _row_secondary_vct]
+_row_vct = _std.vector[_std.tuple[_row_primary_vct, _row_secondary_vct]]
 
 
-def S(x: List[AbstrItem]):
-    return secondary_vct(x)
+def _P(x: List[Tuple[_AbstrItem, int, int]]):
+    return _primary_vct([_primary_tpl(obj, low, high) for (obj, low, high) in x])
 
 
-def RP(x: List[AbstrItem]):
-    return row_primary_vct(x)
+def _S(x: List[_AbstrItem]):
+    return _secondary_vct(x)
 
 
-def RS(x: List[Tuple[AbstrItem, int]]):
-    return row_secondary_vct([row_secondary_tpl(obj, clr) for (obj, clr) in x])
+def _RP(x: List[_AbstrItem]):
+    return _row_primary_vct(x)
 
 
-def R(x: List[Tuple[List[AbstrItem], List[Tuple[AbstrItem, int]]]]):
-    return row_vct([row_tpl(RP(p), RS(s)) for (p, s) in x])
+def _RS(x: List[Tuple[_AbstrItem, int]]):
+    return _row_secondary_vct([_row_secondary_tpl(obj, clr) for (obj, clr) in x])
 
 
-def new_id_generator():
+def _R(x: List[Tuple[List[_AbstrItem], List[Tuple[_AbstrItem, int]]]]):
+    return _row_vct([_row_tpl(_RP(p), _RS(s)) for (p, s) in x])
+
+
+def _new_id_generator():
     x = -1
 
     def f():
@@ -53,7 +53,7 @@ def new_id_generator():
     return f
 
 
-class ConcItem(AbstrItem):
+class ConcItem(_AbstrItem):
     def set(self, x):
         self.x = x
 
@@ -97,7 +97,7 @@ class DLXM():
         self.primary_items = {}
         self.secondary_items = {}
         self.variables = []
-        self.new_id = new_id_generator()
+        self.new_id = _new_id_generator()
         self.rows = []
         self.rows_cpp = []
 
@@ -109,7 +109,7 @@ class DLXM():
     def add_row(self, row_primary: List[ConcItem] = [],
                 row_secondary: List[Tuple[ConcItem, int]] = []):
         self.rows.append((row_primary, row_secondary))
-        self.rows_cpp.append((RP(row_primary), RS(row_secondary)))
+        self.rows_cpp.append((_RP(row_primary), _RS(row_secondary)))
 
     def row(self, i):
         p, s = self.rows[i]
@@ -128,11 +128,11 @@ class DLXM():
                 for k in x:
                     primary_items.append((x[k], x.lower_bound, x.upper_bound))
 
-        primary = P(primary_items)
-        secondary = S(secondary_items)
-        rows = R([])
+        primary = _P(primary_items)
+        secondary = _S(secondary_items)
+        rows = _R([])
 
-        dlx = DLX(primary, secondary, rows)
+        dlx = _DLX(primary, secondary, rows)
         for p, s in self.rows_cpp:
             dlx.add_row(p, s)
         sols = dlx.all_solutions(verbose)
@@ -145,7 +145,10 @@ if __name__ == "__main__":
             print("Solution", sol, ":")
             for i in sol:
                 for e in dlx.row(i):
-                    print(e, end=" ")
+                    if type(e) == tuple:
+                        print(e[0] + ":" + str(e[1]), end=" ")
+                    else:
+                        print(e, end=" ")
                 print("")
         print("--\n%d solutions" % len(sols))
 
